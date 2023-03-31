2954655d33bd0b9ba0404b81d1bceeecb890ee06
dramob <71010379+dramob@users.noreply.github.com>
2023-03-31 13:54:23 +0200

    Update NotreEquipe.js

--- frontend/pages/NotreEquipe.js
+++ frontend/pages/NotreEquipe.js
@@ -1,9 +1,13 @@
-// components/NotreEquipe.js
+// Importation de React et des styles du fichier '../styles/NotreEquipe.module.css'
 import React from 'react';
 import styles from '../styles/NotreEquipe.module.css';
+
+// Importation du composant Header depuis le fichier '../components/Header'
 import Header from '../components/Header';
 
+// Définition d'une fonction fléchée nommée NotreEquipe
 const NotreEquipe = () => {
+  // Définition d'un tableau d'objets contenant les informations des membres de l'équipe
   const teamMembers = [
     { id: 1, name: 'Alia', description: 'Développeur front', photo: '/alia.jpeg' },
     { id: 2, name: 'Neil', description: 'Developpeur blockchain', photo: '/neil.jpeg' },
@@ -11,10 +15,14 @@ const NotreEquipe = () => {
     { id: 4, name: 'Adnene', description: 'Developpeur blockhain', photo: '/adnene.jpeg' },
   ];
 
+  // La fonction NotreEquipe retourne le contenu JSX suivant :
   return (
     <div className={styles.content}>
+      {/* Affichage du composant Header avec un titre et un sous-titre */}
       <Header title="Notre équipe" subtitle="Rencontrez nos membres" />
+      {/* Utilisation d'une grille CSS pour afficher les membres de l'équipe */}
       <div className={styles.teamGrid}>
+        {/* Boucle de rendu pour chaque membre de l'équipe */}
         {teamMembers.map((member) => (
           <div key={member.id} className={styles.teamMember}>
             <img className={styles.memberPhoto} src={member.photo} alt={member.name} />
@@ -27,4 +35,5 @@ const NotreEquipe = () => {
   );
 };
 
+// Exportation du composant NotreEquipe par défaut
 export default NotreEquipe;
