2ae89f4cbd2d40b6e94045ccb597ded8e7ca4df2
dramob <71010379+dramob@users.noreply.github.com>
2023-03-31 13:50:25 +0200

    Update mainLayout.jsx

--- frontend/layout/mainLayout.jsx
+++ frontend/layout/mainLayout.jsx
@@ -1,17 +1,26 @@
+
+// Importation du composant Navbar depuis le fichier '../components/navigation/navbar'
 import Navbar from "../components/navigation/navbar";
+
+// Importation du composant Header depuis le fichier '../components/Header'
 import { Header } from "../components/Header";
+
+// Importation du composant Footer depuis le fichier '../components/Footer'
 import Footer from "../components/Footer";
+
+// Définition d'une fonction MainLayout qui prend un paramètre 'children'
 export default function MainLayout({ children }) {
+	// La fonction MainLayout retourne le contenu JSX suivant :
 	return (
 		<div>
-			
+			{/* Affichage du composant Navbar */}
             <Navbar />
-            
-			{children}
-			
-			
-			<Footer/>
 
+			{/* Affichage du contenu passé en paramètre de la fonction MainLayout */}
+			{children}
+
+			{/* Affichage du composant Footer */}
+			<Footer/>
 		</div>
 	);
 }
