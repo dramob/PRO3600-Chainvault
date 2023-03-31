d00368c5383236326271bc3a43bc6b371f1cbf34
dramob <71010379+dramob@users.noreply.github.com>
2023-03-31 13:53:15 +0200

    comment

--- frontend/pages/APropos.js
+++ frontend/pages/APropos.js
@@ -1,11 +1,17 @@
-// APropos.js
+// Importation de React et des styles du fichier '../styles/APropos.module.css'
 import React from 'react';
 import styles from '../styles/APropos.module.css';
+
+// Importation du composant Header depuis le fichier '../components/Header'
 import Header from '../components/Header';
+
+// Définition d'une fonction  APropos
 const APropos = () => {
   return (
     <div className={styles.content}>
+      {/* Affichage du composant Header avec un titre et un sous-titre */}
       <Header title="À propos" subtitle="Information sur notre entreprise et notre équipe." />
+      {/* Paragraphe expliquant comment la technologie blockchain est utilisée pour stocker et échanger des documents sensibles en toute sécurité */}
       <p className={styles.blockchain}>
         Notre plateforme utilise la technologie blockchain pour authentifier la possession de documents sensibles. 
         Grâce à la décentralisation et à la sécurité offertes par la blockchain, nous pouvons garantir que les documents sont 
@@ -16,4 +22,5 @@ const APropos = () => {
   );
 };
 
+// Exportation du composant APropos par défaut
 export default APropos;
