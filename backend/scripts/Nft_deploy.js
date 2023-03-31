6907f5b8f03f481a78b867acea61ddb65ee77ddf
dramob <71010379+dramob@users.noreply.github.com>
2023-03-31 14:04:17 +0200

    Update Nft_deploy.js

--- backend/scripts/Nft_deploy.js
+++ backend/scripts/Nft_deploy.js
@@ -1,15 +1,18 @@
-const hre = require("hardhat");
+Const hre = require("hardhat");
 
 async function main() {
-	const Contract = await hre.ethers.getContractFactory("Nft");
-	const contract = await Contract.deploy();
+  // Obtention de l'usine de contrat NFT
+  const Contract = await hre.ethers.getContractFactory("Nft");
+  // Déploiement du contrat
+  const contract = await Contract.deploy();
+  // Attendre que le contrat soit déployé
+  await contract.deployed();
 
-	await contract.deployed();
-
-	console.log("Nft deployed to:", contract.address);
+  console.log("Nft deployed to:", contract.address);
 }
 
+// Appel de la fonction principale et gestion des erreurs
 main().catch((error) => {
-	console.error(error);
-	process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
